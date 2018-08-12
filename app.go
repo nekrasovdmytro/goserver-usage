33287658364ca711458a92f42b2f9ec870e8f093
Dmytro Nekrasov <nekrasov.dmytro@gmail.com>
2018-08-12 18:46:40 +0200

    go server usage init

---
+++ app.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"awesomeProject/src/github.com/nekrasovdmytro/goserver"
+
+	"net/http"
+)
+
+func main() {
+	var routers []goserver.Router
+
+	routerCollector := goserver.RouterCollector{
+		Routers: routers,
+	}
+
+	routerCollector.AddRouter(goserver.Router{Path: "/hello", Func: func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	}})
+
+	server := goserver.Server{routerCollector}
+
+	server.Run()
+}
